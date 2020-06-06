0e0efe16a166446d1c2f91bfc525d13ff3ed566a
Woojae Gabriel Lim <dalpengholic@gmail.com>
2020-06-06 15:47:16 +0300

    after #2.12 Recap

---
+++ controllers/videoController.js
@@ -0,0 +1,7 @@
+export const home = (rew, res) => res.send("Home");
+export const search = (rew, res) => res.send("Search");
+export const videos = (rew, res) => res.send("Videos");
+export const upload = (rew, res) => res.send("Upload");
+export const videoDetail = (rew, res) => res.send("Video Detail");
+export const editVideo = (rew, res) => res.send("Edit Video");
+export const deleteVideo = (rew, res) => res.send("Delete Video");
