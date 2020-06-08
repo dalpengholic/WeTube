378711424d8b9d4a06579162923e1b359d47436d
Woojae Gabriel Lim <dalpengholic@gmail.com>
2020-06-08 14:06:02 +0300

    after #2.21

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # WeTube
 Cloning YouTube with Vanilla Js and NodeJS
+
+## Pages:
+
+- [] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [] User Detail
+- [] Change Password
+- [] Upload
+- [] Video Detail
+- [] Edit Video
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
-export const home = (req, res) => res.render("home", {pageTitle:"Home"});
+import {videos} from "../db";
+
+export const home = (req, res) => res.render("home", {pageTitle:"Home", videos});
 
 export const search = (req, res) => {
     const {query: {term:searchingBy}} = req;
@@ -6,8 +8,7 @@ export const search = (req, res) => {
 };
 
 
-export const videos = (req, res) => res.render("Videos", {pageTitle:"Videos"});
 export const upload = (req, res) => res.render("Upload", {pageTitle:"Upload"});
 export const videoDetail = (req, res) => res.render("Video Detail", {pageTitle:"Video Detail"});
 export const editVideo = (req, res) => res.render("Edit Video", {pageTitle:"Edit Video"});
-export const deleteVideo = (req, res) => res.render("Delete Video", {pageTitle:"Delete Video"});  
+export const deleteVideo = (req, res) => res.render("Delete Video", {pageTitle:"Delete Video"});   
