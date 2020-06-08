609c7c5618257df9d52db72e71cb6b34f26b6707
Woojae Gabriel Lim <dalpengholic@gmail.com>
2020-06-08 20:30:07 +0300

    end of Express

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,8 +21,23 @@ export const postJoin = (req,res) => {
 }
 
 
-export const login = (req,res) => res.render('login',{ pageTitle: "Log In"});
-export const logout = (req,res) => res.render('logout',{ pageTitle: "Log In"});
+export const getLogin = (req,res) => {
+    res.render('login',{ pageTitle: "Log In"});
+};
+export const postLogin = (req,res) => {
+    res.redirect(routes.home);
+};
+
+
+
+export const logout = (req,res) => {
+    res.redirect(routes.home);
+}
+
+
+
+
+
 export const userDetail = (req,res) => res.render('userDetail',{ pageTitle: "User Detail"});
 export const editProfile = (req,res) => res.render('editProfile',{ pageTitle: "Edit Profile"});
 export const changePassword = (req,res) => res.render('changePassword',{ pageTitle: "Chagne Password"});
