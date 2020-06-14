6b91aaea24e90e9a2438c81d29fecf5442b34f9b
Woojae Gabriel Lim <dalpengholic@gmail.com>
2020-06-14 21:04:07 +0300

    after #6.4

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,12 @@
-import routes from "../routes"
+import passport from "passport";
+import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req,res) => {
     res.render('join',{ pageTitle: "Join"});
 }; 
 
-export const postJoin = (req,res) => {
+export const postJoin = async (req,res, next) => {
     const {
         body: {name, email, password, password2}
     } = req;
@@ -13,9 +15,18 @@ export const postJoin = (req,res) => {
         res.status(400);
         res.render("join", { pageTitle: "Join"});
     } else{
-        // TO DO : resgister user
-        // To DO : log user in
-        res.redirect(routes.home);
+        try{
+            const user = await User({
+                name, 
+                email
+            });
+            await  User.register(user, password);
+            next();
+        } catch(error){
+            console.log(error);
+            res.redirect(routes.home);
+        }
+
     }
     res.render('join',{ pageTitle: "Join"});
 }
@@ -24,10 +35,10 @@ export const postJoin = (req,res) => {
 export const getLogin = (req,res) => {
     res.render('login',{ pageTitle: "Log In"});
 };
-export const postLogin = (req,res) => {
-    res.redirect(routes.home);
-};
-
+export const postLogin = passport.authenticate('local', {
+    failureRedirect: routes.login,
+    successRedirect: routes.home
+});
 
 
 export const logout = (req,res) => {
