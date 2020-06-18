31b1e75b1bdbb56966f9a3b9947867ce41e3661b
Woojae Gabriel Lim <dalpengholic@gmail.com>
2020-06-18 15:28:08 +0300

    ongoing #7.0

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,6 @@ export const postJoin = async (req,res, next) => {
             console.log(error);
             res.redirect(routes.home);
         }
-
     }
     res.render('join',{ pageTitle: "Join"});
 }
@@ -40,15 +39,107 @@ export const postLogin = passport.authenticate('local', {
     successRedirect: routes.home
 });
 
+export const githubLogin = passport.authenticate("github");
+
+export const githubLoginCallback = async (_, __, profile, cb) => {
+    const { 
+        _json: {id, avatar_url: avatarUrl, name, email} 
+        } = profile;
+
+    try{
+        const user = await User.findOne({email});
+        if(user){
+            user.githubId = id;
+            user.save();
+            return cb(null, user);
+        }
+        const newUser = await User.create({
+            email, 
+            name, 
+            githubId: id,
+            avatarUrl
+        });
+        return cb(null, newUser);
+    } catch(error){
+        return cb(error);
+    }
+};
+
+export const postsGitHubLogIn = (req,res) => {
+    res.redirect(routes.home);
+};
+
+
+export const facebookLogin = passport.authenticate("facebook");
+
+export const facebookLoginCallback = async (_, __, profile, cb) => {
+    const {_json: {id, name, email}} = profile;
+
+    try{
+        const user = await User.findOne({email});
+        if(user){
+            user.facebookId = id;
+            user.avatarUrl = `https://graph.facebook.com/${id}/picture?type=large`;
+            user.save();
+            return cb(null, user);
+        }
+        const newUser = await User.create({
+            email, 
+            name, 
+            facebookId: id,
+            avatarUrl: `https://graph.facebook.com/${id}/picture?type=large`
+        });
+        return cb(null, newUser);
+
+    }catch (error){
+        return cb(error);
+
+    };
+};
+
+export const postFacebookLogIn = (req, res) => {
+    res.redirect(routes.home);
+};
+
 
 export const logout = (req,res) => {
+    req.logout();
     res.redirect(routes.home);
-}
+};
+
+export const getMe = (req,res) => {
+    res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+};
 
 
+export const userDetail = async(req,res) => {
+    const{ params: { id } } = req;
+    try {
+        const user = await User.findById(id);
+        res.render('userDetail',{ pageTitle: "User Detail", user});
+    } catch (error) {
+        res.redirect(routes.home);
+    }
+};
 
 
+export const getEditProfile = (req,res) => res.render('editProfile',{ pageTitle: "Edit Profile"});
 
-export const userDetail = (req,res) => res.render('userDetail',{ pageTitle: "User Detail"});
-export const editProfile = (req,res) => res.render('editProfile',{ pageTitle: "Edit Profile"});
+export const postEditProfile = async (req, res) => {
+    const {
+      body: { name, email },
+      file
+    } = req;
+    try {
+      await User.findByIdAndUpdate(req.user.id, {
+        name,
+        email,
+        avatarUrl: file ? file.path : req.user.avatarUrl
+      });
+      res.redirect(routes.me);
+    } catch (error) {
+      res.render("editProfile", { pageTitle: "Edit Profile" });
+    }
+  };
+
 export const changePassword = (req,res) => res.render('changePassword',{ pageTitle: "Chagne Password"});
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,24 +1,47 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
+
 import {home, search} from "../controllers/videoController";
 import {
     getJoin, 
     getLogin,
     logout,
     postLogin,
-    postJoin} from "../controllers/userController";
+    postJoin,
+    githubLogin,
+    postsGitHubLogIn,
+    getMe,
+    facebookLogin,
+    postFacebookLogIn} from "../controllers/userController";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
 globalRouter.get(routes.search, search);
+
+globalRouter.get(routes.gitHub, githubLogin);
+globalRouter.get(
+    routes.gitHubCallback, 
+    passport.authenticate('github',{ failtureRedirect: "/login"}),
+    postsGitHubLogIn
+);
+
+globalRouter.get(routes.me, getMe);
+
+globalRouter.get(routes.facebook, facebookLogin);
+globalRouter.get(
+    routes.facebookCallback, 
+    passport.authenticate('facebook', { failureRedirect: "/login"}),
+    postFacebookLogIn)
 
 export default globalRouter;
