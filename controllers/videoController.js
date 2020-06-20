6c532fb5fa7bbe59bc41eae4659615735002fcd2
Woojae Gabriel Lim <dalpengholic@gmail.com>
2020-06-20 14:34:30 +0300

    Revert "broken something at #7.4"
    This reverts commit d7fbfdf36d4e40a504793970992360ff4ffe4cd8.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -59,6 +59,7 @@ export const videoDetail = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id).populate("creator");
+    console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
@@ -73,11 +74,7 @@ export const getEditVideo = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    if (video.creator !== req.user.id) {
-      throw Error();
-    } else {
-      res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
-    }
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
   } catch (error) {
     res.redirect(routes.home);
   }
@@ -103,12 +100,7 @@ export const deleteVideo = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id);
-    if (video.creator !== req.user.id) {
-      throw Error();
-    } else {
-      await Video.findOneAndRemove({ _id: id });
-    }
+    await Video.findOneAndRemove({ _id: id });
   } catch (error) {
     console.log(error);
   }
